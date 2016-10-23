a6d6aa222a30664a649d4700dbea61e9498a874c
jonasguan <jonas.guan@gmail.com>
2016-10-23 16:51:24 -0700

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,27 @@
+import media
+
+wreck_it_ralph = media.Movie( 	"Wreck-it Ralph",
+				"A disliked video game character gains popularity by wrecking things.",
+				"https://upload.wikimedia.org/wikipedia/en/1/15/Wreckitralphposter.jpeg",
+				"https://www.youtube.com/watch?v=OzMJkRwOxjs")
+
+the_dark_knight = media.Movie( 	"The Dark Knight",
+				"Batman faces a new supervillian who throws Gotham into utter chaos.",
+				"https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
+				"https://www.youtube.com/watch?v=u843KNE-exo")
+
+interstellar = media.Movie(	"Interstellar",
+				"A group of scientists journey into space to save mankind from the brink of extinction.",
+				"https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+				"https://www.youtube.com/watch?v=lZMzf-SDWP8")
+
+the_wolf_of_wall_street = media.Movie(	"The Wolf of Wall Street",
+					"Based on the real life of Jordan Belfort, a man rises to wealth but gets" 
+					"ultimately consumed by his hubris.",
+					"https://upload.wikimedia.org/wikipedia/en/1/1f/WallStreet2013poster.jpg",
+					"https://www.youtube.com/watch?v=BzpIB5TJ7LI")
+
+pulp_fiction = media.Movie(	"Pulp Fiction",
+				"Several storylines of bad motherfuckers intertwine in a black comedy masterpiece.",
+				"https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Pulp_Fiction_%281994%29_poster.jpg/220px-Pulp_Fiction_%281994%29_poster.jpg",
+				"https://www.youtube.com/watch?v=t7cuDVbN_mg")
---
+++ runme.py
@@ -0,0 +1,7 @@
+import media
+import fresh_tomatoes
+import entertainment_center
+
+movies = [entertainment_center.wreck_it_ralph, entertainment_center.the_dark_knight, entertainment_center.interstellar, entertainment_center.the_wolf_of_wall_street, entertainment_center.pulp_fiction]
+
+fresh_tomatoes.open_movies_page(movies)
